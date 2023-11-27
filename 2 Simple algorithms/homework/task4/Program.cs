1b590154b74dcefc5088feba011c230b44eda9c6
Oliasinkevich <olga.sinkevich89@gmail.com>
2023-11-27 20:29:11 +0300

    2 Simple algorithms: homework task 4 corrected

--- 2 Simple algorithms/homework/task4/Program.cs
+++ 2 Simple algorithms/homework/task4/Program.cs
@@ -1,8 +1,22 @@
 ﻿// Задача 4: Напишите программу, которая на вход принимает натуральное число N, а на выходе показывает его цифры через запятую.
 
-Console.Write("Введите число: ");
-int number = Convert.ToInt32(System.Console.ReadLine());
+// Console.Write("Введите число: ");
+// int number = Convert.ToInt32(System.Console.ReadLine());
 
-string digits = string.Join(", ", number.ToString().ToCharArray());
-Console.WriteLine(digits);
+// string digits = string.Join(", ", number.ToString().ToCharArray());
+// Console.WriteLine(digits);
+
+// или
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int first = number;
+string result = string.Empty;
+for (; number > 0; number /= 10)
+{
+    if (number == first) result = number % 10 + " " + result;
+    else result = number % 10 + ", " + result;
+}
+
+Console.Write(result);
 
